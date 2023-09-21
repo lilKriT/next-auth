de330fc2794f6c096bb53b349a90ad1b30d112e8
Piotr Filipiak <kiddhkane@gmail.com>
2023-09-21 03:43:10 +0200

    Created a basic auth route.

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "Your username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "Your password",
+        },
+      },
+      async authorize(credentials) {
+        // Here you would get your data from database!
+        const user = { id: "1", name: "Peter", password: "123" };
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  // you can also add pages, session...
+});
+
+export { handler as GET, handler as POST };
