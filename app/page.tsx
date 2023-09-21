af5ff0ac7c57744a78d90414faefeb12fc409468
Piotr Filipiak <kiddhkane@gmail.com>
2023-09-21 06:57:30 +0200

    Home page uses session.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { options } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession(options);
+
   return (
     <main className="">
       <h1>Home page.</h1>
+      {session ? "You are logged in." : "NOT logged in."}
     </main>
   );
 }
