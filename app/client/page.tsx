f34c991c176dd73e87b01ecef141465962cc90de
Piotr Filipiak <kiddhkane@gmail.com>
2023-09-21 06:56:42 +0200

    Created client and server pages.

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import UserCard from "../components/UserCard";
 
 const ClientPage = () => {
-  return <div>ClientPage</div>;
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/api/auth/signin?callbackUrl=/client");
+    },
+  });
+
+  return (
+    <div>
+      <UserCard user={session?.user} pagetype={"Client"} />
+    </div>
+  );
 };
 
 export default ClientPage;
--- app/server/page.tsx
+++ app/server/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const ServerPage = () => {
-  return <div>ServerPage</div>;
+import { options } from "../api/auth/[...nextauth]/options";
+import UserCard from "../components/UserCard";
+const ServerPage = async () => {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/server");
+  }
+
+  return (
+    <div>
+      This is a server page.
+      <UserCard user={session.user} pagetype="server" />
+    </div>
+  );
 };
 
 export default ServerPage;
